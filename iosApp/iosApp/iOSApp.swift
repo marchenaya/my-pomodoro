5f1ffbda0868cbcd4d6a636908b373f5a5b20270
Anna Marchenay <anna.marchenay@appsolute.fr>
2026-06-01 16:50:03 +0200

    Initialize Koin dependency injection for iOS
    - Create a `KoinIos` helper object in the iOS source set to provide a Koin initialization entry point for Swift.
    - Update the `iOSApp` entry point to call the Koin initialization logic during application startup.

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,7 +1,12 @@
 import SwiftUI
+import Shared
 
 @main
 struct iOSApp: App {
+    init() {
+        KoinIos.shared.initialize()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
--- shared/src/iosMain/kotlin/com/marchenaya/mypomodoro/MainViewController.kt
+++ shared/src/iosMain/kotlin/com/marchenaya/mypomodoro/MainViewController.kt
@@ -2,5 +2,12 @@ package com.marchenaya.mypomodoro
 
 import androidx.compose.ui.window.ComposeUIViewController
 import com.marchenaya.mypomodoro.app.App
+import com.marchenaya.mypomodoro.di.initKoin as initKoinCommon
 
 fun MainViewController() = ComposeUIViewController { App() }
+
+object KoinIos {
+    fun initialize() {
+        initKoinCommon { }
+    }
+}
